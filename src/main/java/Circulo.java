f55ffed243393c6f6ffc7427e4e616bc4b8fdd60
kaik aciole <kaikaciole1.0@gmail.com>
2023-09-04 16:30:22 -0300

    finalizei estudante

---
+++ src/main/java/Circulo.java
@@ -0,0 +1,21 @@
+public class Circulo {
+    float raio;
+    float circunferencia;
+    float area; 
+
+
+
+
+    public Circulo(float raio, float circunferencia, float area){
+        this.raio = raio;
+        this.circunferencia = circunferencia;
+
+
+    }
+
+
+
+
+
+
+}
