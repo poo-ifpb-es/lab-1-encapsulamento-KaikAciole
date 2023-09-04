6bb15f6fcd08cd4ac88b0c6004b7f2e7503829ca
kaik aciole <kaikaciole1.0@gmail.com>
2023-09-04 16:56:04 -0300

    salvando para terminar em casa

--- src/main/java/Circulo.java
+++ src/main/java/Circulo.java
@@ -2,20 +2,31 @@ public class Circulo {
     float raio;
     float circunferencia;
     float area; 
-
-
-
-
-    public Circulo(float raio, float circunferencia, float area){
+    float pi = 3.14159f;
+    
+    
+    
+    public Circulo(float raio){
         this.raio = raio;
-        this.circunferencia = circunferencia;
+        this.area = this.raio*pi;
+    }
 
+    public float getRaio(){
+        return this.raio;
+    }
 
+    public void setRaio(float raio){
+        this.raio = raio;
     }
 
 
+    public float getArea(){
+        return this.area;
+    }
 
-
+    public float getCircunferencia(){
+        return this.circunferencia;
+    }
 
 
 }
